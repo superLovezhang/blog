fe991a85319bab4e3beab2503732ffe69d680320
张诏 <2321885538@qq.com>
2021-09-09 22:07:49 +0800

    accomplished the left and the middle part

--- src/component/category/index.tsx
+++ src/component/category/index.tsx
@@ -2,7 +2,75 @@ import { FC } from "react"
 import styles from './index.module.less'
 
 const Category: FC = () => {
-    return <div className={styles.category_wrap}>category...</div>
+    return <div className={styles.category_wrap}>
+        <div className={styles.category_top}>
+            <h3>首页</h3>
+        </div>
+        <div className={`${styles.category_list} ${styles.list}`}>
+            <div className={`${styles.category_item} ${styles.item}`}>
+                <i className={'iconfont icon-biaoqiankuozhan_tuijian-126'}></i>
+                <span>推荐</span>
+            </div>
+            <div className={`${styles.category_item} ${styles.item}`}>
+                <i className={'iconfont icon-zixunzhongxin'}></i>
+                <span>资讯</span>
+            </div>
+            <div className={`${styles.category_item} ${styles.item}`}>
+                <i className={'iconfont icon-_qianduankaifa'}></i>
+                <span>前端</span>
+            </div>
+            <div className={`${styles.category_item} ${styles.item}`}>
+                <i className={'iconfont icon-code'}></i>
+                <span>后端</span>
+            </div>
+            <div className={`${styles.category_item} ${styles.item}`}>
+                <i className={'iconfont icon-dengpao'}></i>
+                <span>人生感悟</span>
+            </div>
+            <div className={`${styles.category_item} ${styles.item}`}>
+                <i className={'iconfont icon-wodewenzhang'}></i>
+                <span>阅读</span>
+            </div>
+        </div>
+        <div className={`${styles.tag_list} ${styles.list}`}>
+            <div className={`${styles.tag_item} ${styles.item}`}>
+                <i>#</i>
+                <span>vue.js</span>
+            </div>
+            <div className={`${styles.tag_item} ${styles.item}`}>
+                <i>#</i>
+                <span>css</span>
+            </div>
+            <div className={`${styles.tag_item} ${styles.item}`}>
+                <i>#</i>
+                <span>html</span>
+            </div>
+            <div className={`${styles.tag_item} ${styles.item}`}>
+                <i>#</i>
+                <span>nodejs</span>
+            </div>
+            <div className={`${styles.tag_item} ${styles.item}`}>
+                <i>#</i>
+                <span>npm</span>
+            </div>
+            <div className={`${styles.tag_item} ${styles.item}`}>
+                <i>#</i>
+                <span>nginx</span>
+            </div>
+            <div className={`${styles.tag_item} ${styles.item}`}>
+                <i>#</i>
+                <span>vue2</span>
+            </div>
+            <div className={`${styles.tag_item} ${styles.item}`}>
+                <i>#</i>
+                <span>vue3</span>
+            </div>
+            <div className={`${styles.tag_item} ${styles.item}`}>
+                <i>#</i>
+                <span>javascript</span>
+            </div>
+        </div>
+    </div>
 }
 
 export default Category

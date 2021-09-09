4b2d7204664e59849c39c52377a3a2c28b67c9cf
张诏 <2321885538@qq.com>
2021-09-09 22:23:25 +0800

    initially complete the right sidebar

--- src/component/articleShortcut/index.tsx
+++ src/component/articleShortcut/index.tsx
@@ -5,7 +5,21 @@ interface ArticleShortcutProps {
 
 }
 const ArticleShortcut: FC<ArticleShortcutProps> = () => {
-    return <div className={styles.article_shortcut_wrap}>articleShortcut...</div>
+    return <div className={styles.article_shortcut_wrap}>
+        <div className={styles.shortcut_recommendation}>
+            <div className={styles.title}>
+                <i className={'iconfont icon-like-fill'}></i>
+                <span>今日推荐</span>
+            </div>
+
+        </div>
+        <div className={styles.shortcut_collection}>
+            <div className={styles.title}>
+                <i className={'iconfont icon-collection-fill'}></i>
+                <span>我的最新收藏</span>
+            </div>
+        </div>
+    </div>
 }
 
 export default ArticleShortcut

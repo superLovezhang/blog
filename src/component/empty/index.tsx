d25422279f6fd6dd0119a712cf907cf2d916213c
张诏 <2321885538@qq.com>
2021-09-11 10:09:28 +0800

    add the tip property to the EmptyProps

--- src/component/empty/index.tsx
+++ src/component/empty/index.tsx
@@ -2,13 +2,13 @@ import { FC } from "react"
 import styles from './index.module.less'
 
 interface EmptyProps {
-
+    tip?: string
 }
-const Empty: FC<EmptyProps> = () => {
+const Empty: FC<EmptyProps> = ({ tip }) => {
     return <div className={styles.empty_wrap}>
         <div className={styles.empty_tip}>
             <i className="iconfont icon-xingqiu"></i>
-            <div>暂无数据</div>
+            <div>{tip ?? '暂无数据'}</div>
         </div>
     </div>
 }

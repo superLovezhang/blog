94ff25894e4baea5639d8e61bbd0b162f256f067
张诏 <2321885538@qq.com>
2021-09-13 11:47:28 +0800

    added the scrollToBottom function

--- src/component/share/index.tsx
+++ src/component/share/index.tsx
@@ -1,8 +1,12 @@
 import styles from './index.module.less'
 
 const Share = () => {
+    const scrollToBottom = () => {
+        window.scrollTo({ top: document?.querySelector('#root')?.clientHeight ?? 0, behavior: 'smooth' })
+    }
+
     return <div className={styles.share_wrap}>
-        <div className={`${styles.comment} ${styles.share_item}`}>
+        <div className={`${styles.comment} ${styles.share_item}`} onClick={scrollToBottom}>
             <i className="iconfont icon-comment_fill_light"></i>
         </div>
         <span>分享</span>

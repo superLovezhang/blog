e6a92f1f541bb1edfae5cc94202a6920c238ab09
张诏 <2321885538@qq.com>
2021-09-14 16:58:25 +0800

    added the MarkDownEditor component

---
+++ src/component/markdownEditor/index.tsx
@@ -0,0 +1,31 @@
+import React, { FC } from 'react'
+import MarkdownIt from 'markdown-it'
+import MdEditor from 'react-markdown-editor-lite'
+import 'react-markdown-editor-lite/lib/index.css'
+
+const mdParser = new MarkdownIt()
+
+interface MarkdownEditorProps {
+    setMdContent: (content: string) => void
+    setHtmlContent?: (content: string) => void
+    style?: { [property: string]: string }
+}
+const MarkdownEditor: FC<MarkdownEditorProps> = ({ style, setMdContent, setHtmlContent }) => {
+    const onImageUpload = (file: File) => {}
+    const onChange = ({ text, html }:  {
+        text: string;
+        html: string;
+    }) => {
+        setMdContent(text)
+        setHtmlContent?.(html)
+    }
+
+    return <MdEditor
+        style={style}
+        shortcuts
+        renderHTML={(text) => mdParser.render(text)}
+        onChange={onChange}
+    />
+}
+
+export default MarkdownEditor

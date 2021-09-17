22f8569a7a23a874dcd8eb30500a497f0e86bfa9
张诏 <2321885538@qq.com>
2021-09-17 13:35:57 +0800

    added the collection function to the Share component

--- src/component/commentList/index.tsx
+++ src/component/commentList/index.tsx
@@ -1,24 +1,50 @@
 import React, { FC } from "react"
 
+import Empty from "@/component/empty/index.tsx"
+
 import styles from "./index.module.less"
 
+const commentList = [
+    {
+        avatar: 'https://xdlumia.oss-cn-beijing.aliyuncs.com/blog/avatar/default-avatar.png?x-oss-process=image/resize,limit_0,m_fill,w_40,h_40/quality,q_100',
+        username: '管理员',
+        content: '网站做得非常不错！吼吼吼吼吼吼吼吼吼吼吼吼吼吼吼吼和😁',
+        like: 1,
+        createTime: '2021-09-17',
+        children: [
+            {
+                avatar: 'https://xdlumia.oss-cn-beijing.aliyuncs.com/blog/avatar/default-avatar.png?x-oss-process=image/resize,limit_0,m_fill,w_40,h_40/quality,q_100',
+                username: '网站管理者',
+                content: '谢谢！我会继续努力的😁',
+                like: 3,
+                createTime: '2021-09-18',
+            }
+        ]
+    },
+    {
+        avatar: 'https://xdlumia.oss-cn-beijing.aliyuncs.com/blog/avatar/default-avatar.png?x-oss-process=image/resize,limit_0,m_fill,w_40,h_40/quality,q_100',
+        username: '管理员',
+        content: '我有一个建议，就是网页写的简单一点',
+        like: 12,
+        createTime: '2021-09-18'
+    }
+]
 interface CommentListProps {
 
 }
 const CommentList: FC<CommentListProps> = () => {
     return <div className={styles.comment_list_wrap}>
-        {/*<Empty tip={'赶快写下您的第一条评论吧'}/>*/}
-        <div className={styles.comment_item}>
+        {commentList.length > 0 ? commentList.map(comment => <div className={styles.comment_item}>
             <div className={styles.comment_avatar}>
-                <img src="https://xdlumia.oss-cn-beijing.aliyuncs.com/blog/avatar/default-avatar.png?x-oss-process=image/resize,limit_0,m_fill,w_40,h_40/quality,q_100" alt=""/>
+                <img src={comment.avatar} alt=""/>
             </div>
             <div className={styles.comment_main}>
-                <div className={styles.comment_username}>管理员</div>
-                <div className={styles.comment_content}>网站做得非常不错！吼吼吼吼吼吼吼吼吼吼吼吼吼吼吼吼和😁</div>
+                <div className={styles.comment_username}>{comment.username}</div>
+                <div className={styles.comment_content}>{comment.content}</div>
                 <div className={styles.comment_operation}>
                     <div className={styles.like}>
                         <i className='iconfont icon-like-fill'></i>
-                        <span>1点赞</span>
+                        <span>{comment.like} 点赞</span>
                     </div>
                     <div className={styles.reply}>
                         <i className='iconfont icon-a-share3-fill'></i>
@@ -26,8 +52,9 @@ const CommentList: FC<CommentListProps> = () => {
                     </div>
                 </div>
             </div>
-            <div className={styles.comment_date}>2021-09-17</div>
-        </div>
+            <div className={styles.comment_date}>{comment.createTime}</div>
+        </div>) : <Empty tip={'赶快写下您的第一条评论吧'}/>}
+
     </div>
 }
 
--- src/component/share/index.tsx
+++ src/component/share/index.tsx
@@ -6,14 +6,21 @@ const Share = () => {
     }
 
     return <div className={styles.share_wrap}>
-        <div className={`${styles.comment} ${styles.share_item}`} onClick={scrollToBottom}>
+        <div className={`${styles.share_item}`} title='收藏'>
+            <i className="iconfont icon-collected"></i>
+        </div>
+        <div
+            className={`${styles.comment} ${styles.share_item}`}
+            onClick={scrollToBottom}
+            title='评论'
+        >
             <i className="iconfont icon-comment_fill_light"></i>
         </div>
         <span>分享</span>
-        <div className={`${styles.weibo} ${styles.share_item}`}>
+        <div className={`${styles.weibo} ${styles.share_item}`} title='微博'>
             <i className="iconfont icon-weibo"></i>
         </div>
-        <div className={`${styles.zone} ${styles.share_item}`}>
+        <div className={`${styles.zone} ${styles.share_item}`} title='QQ空间'>
             <i className="iconfont icon-qzone"></i>
         </div>
     </div>

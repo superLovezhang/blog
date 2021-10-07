82b748a7d4c1dba66d37589d32adc8d8f22b2cc6
张诏 <2321885538@qq.com>
2021-10-07 11:23:38 +0800

    accomplished the form validation function

--- src/component/login/index.tsx
+++ src/component/login/index.tsx
@@ -1,6 +1,97 @@
-import { FC, useState } from "react"
+import { FC, ReactElement, useState } from "react"
 import { useForm } from 'react-hook-form'
 import styles from './index.module.less'
+import { RegisterOptions } from "react-hook-form/dist/types/validator"
+import {FieldError} from "react-hook-form/dist/types/errors";
+
+interface Field {
+    className?: string
+    iconClassName?: string
+    fieldName: string
+    type: string
+    placeholder: string
+    keyword: string
+    optional?: (fields: any) => RegisterOptions
+    children?: ReactElement
+}
+
+const LOGIN_FIELDS: Field[] = [
+    {
+        className: styles.login_input,
+        iconClassName: 'icon-youjianyouxiang',
+        type: 'text',
+        fieldName: 'email',
+        placeholder: '请输入邮箱',
+        keyword: '邮箱',
+        optional: (fields?: any) => ({})
+    },
+    {
+        className: styles.login_input,
+        iconClassName: 'icon-lock1',
+        fieldName: 'password',
+        type: 'password',
+        placeholder: '请输入密码',
+        keyword: '密码',
+        optional: (fields?: any) => ({})
+    }
+]
+const REGISTER_FIELDS: Field[] = [
+    {
+        className: styles.login_input,
+        iconClassName: 'icon-youjianyouxiang',
+        type: 'text',
+        fieldName: 'email',
+        placeholder: '请输入邮箱',
+        keyword: '邮箱',
+        optional: (fields?: any) => ({
+            pattern: /\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*/,
+            required: true
+        })
+    },
+    {
+        className: `${styles.login_input} ${styles.verify_code}`,
+        iconClassName: 'icon-security',
+        fieldName: 'verifyCode',
+        type: 'text',
+        placeholder: '请输入验证码',
+        keyword: '验证码',
+        optional: (fields?: any) => ({
+            maxLength: 6,
+            pattern: /^\d+$/,
+            required: true
+        }),
+        children: <button>获取验证码</button>
+    },
+    {
+        className: `${styles.login_input}`,
+        fieldName: 'username',
+        type: 'text',
+        placeholder: '请输入用户昵称',
+        keyword: '用户昵称',
+        optional: (fields?: any) => ({ required: true }),
+    },
+    {
+        className: styles.login_input,
+        iconClassName: 'icon-lock1',
+        type: 'password',
+        fieldName: 'password',
+        placeholder: '请输入密码',
+        keyword: '密码',
+        optional: (fields?: any) => ({ required: true })
+    },
+    {
+        className: styles.login_input,
+        iconClassName: 'icon-lock1',
+        type: 'password',
+        fieldName: 'confirmPassword',
+        placeholder: '请输入确认密码',
+        keyword: '确认密码',
+        optional: (fields?: any) => ({
+            required: true,
+            validate: fields ? (value: any) => fields?.password === value : null
+        })
+    }
+]
 
 interface LoginProps {
     visible?: boolean
@@ -32,48 +123,65 @@ const Login: FC<LoginProps> = ({ visible, setVisible }) => {
     return null
 }
 
+interface BlogInputProps {
+    className?: string,
+    inputProps?: { [key: string]: any},
+    iconClassName?: string,
+    error?: FieldError,
+    children?: ReactElement
+}
+const BlogInput: FC<BlogInputProps> = ({
+                                           className,
+                                           inputProps,
+                                           error,
+                                           iconClassName,
+                                           children
+}) => {
+    const errorTip = () => {
+        return ''
+    }
+
+    return <div className={className}>
+        <input {...inputProps}/>
+        {iconClassName && <i className={`iconfont ${iconClassName}`}/>}
+        <p className={styles.error_tip}>{errorTip()}</p>
+        {children}
+    </div>
+}
+
 const LoginBox = () => {
     return <>
-        <div className={styles.login_input}>
-            <input type="text" placeholder='请输入邮箱'/>
-            <i className={'iconfont icon-youjianyouxiang'}/>
-        </div>
-        <div className={styles.login_input}>
-            <input type="password" placeholder='请输入密码'/>
-            <i className={'iconfont icon-lock1'}/>
-        </div>
+        {LOGIN_FIELDS.map(({ className, type, placeholder, iconClassName, fieldName }) => <BlogInput
+            key={fieldName}
+            className={className}
+            inputProps={{ type: type, placeholder: placeholder }}
+            iconClassName={iconClassName}
+        />)}
         <button>立即登录</button>
     </>
 }
 
 const RegisterBox = () => {
-    const { register, watch, formState: { errors } } = useForm()
-    const registerUser = () => {
-        console.log(watch())
+    const { register, watch, handleSubmit, formState: { errors } } = useForm()
+    const registerUser = (data: any) => {
+        console.log('current user info: ', data, ', and errors is: ', errors)
     }
+    console.log(errors)
 
     return <>
-        <div className={styles.login_input}>
-            <input type="text" placeholder='请输入邮箱' {...register('email', { pattern: /$%#/ })}/>
-            <i className={'iconfont icon-youjianyouxiang'}/>
-        </div>
-        <div className={`${styles.login_input} ${styles.verify_code}`}>
-            <input type="text" placeholder='请输入验证码' {...register('verifyCode', { maxLength: 6, pattern: /\w/ })}/>
-            <i className={'iconfont icon-security'}/>
-            <button>获取验证码</button>
-        </div>
-        <div className={styles.login_input}>
-            <input type="text" placeholder='请输入用户昵称'/>
-        </div>
-        <div className={styles.login_input}>
-            <input type="text" placeholder='请输入密码'/>
-            <i className={'iconfont icon-lock1'}/>
-        </div>
-        <div className={styles.login_input}>
-            <input type="text" placeholder='请确认密码'/>
-            <i className={'iconfont icon-lock1'}/>
-        </div>
-        <button>立即注册</button>
+        {REGISTER_FIELDS.map(({ keyword, className, type, placeholder, iconClassName, optional = () => undefined, children, fieldName }) => <BlogInput
+            key={fieldName}
+            className={className}
+            inputProps={{
+                style: { borderColor: errors[fieldName] && '#f56c6c' },
+                type: type,
+                placeholder: placeholder,
+                ...register(fieldName, optional(watch()))
+            }}
+            iconClassName={iconClassName}
+            children={children}
+        />)}
+        <button onClick={handleSubmit(registerUser)}>立即注册</button>
     </>
 }
 

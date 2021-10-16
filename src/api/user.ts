6e39b151e5a8482a36f9ee4aa3c46c59b9c987df
张诏 <2321885538@qq.com>
2021-10-16 13:16:12 +0800

    added react-query dependency to manage global state throughout the application.

--- src/api/user.ts
+++ src/api/user.ts
@@ -2,5 +2,6 @@ import request from '@/util/request.js'
 import { LoginParams, UserDTO } from "./types"
 
 
-export const login = (params: LoginParams) => request.get('/user/login', { params })
+export const login = (params: LoginParams) => request('/user/login', { params })
+export const userInfo = () => request('/user/userInfo')
 export const register = (data: UserDTO) => request.put('/user/register', data)
---
+++ src/query/userQuery.ts
@@ -0,0 +1,4 @@
+import { useQuery, useMutation } from 'react-query'
+import { login, userInfo, register } from '../api/user'
+
+export const a = 1
